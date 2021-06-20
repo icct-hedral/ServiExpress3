3a79a721f30d2b5fa6a75f8b903a8b08ead52f31
Juan Carlos <jcarlos132213@gmail.com>
2021-06-20 18:57:31 -0500

    modificación de configuraciones para ejecutar BD

--- src/main/java/com/empresa/demo/model/Detalle_comprobante.java
+++ src/main/java/com/empresa/demo/model/Detalle_comprobante.java
@@ -2,26 +2,31 @@ package com.empresa.demo.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "detalle_comprobante")
 public class Detalle_comprobante implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer detalle_com;
+	private Integer id_detalle;
 
 	@NotNull
 	private Integer cantidad;
-	
-	//relacion de muchos apuntado a la entidad producto
+
+	// relacion de muchos apuntado a la entidad producto
 	@ManyToOne(fetch = FetchType.LAZY)
-	//creara un campo en esta entidad, la cual sera la relación que tendremos con la entidad producto
+	// creara un campo en esta entidad, la cual sera la relación que tendremos con
+	// la entidad producto
 	@JoinColumn(name = "id_producto")
 	private Producto producto;
 
@@ -29,18 +34,18 @@ public class Detalle_comprobante implements Serializable {
 
 	}
 
-	public Detalle_comprobante(Integer detalle_com, Integer cantidad) {
+	public Detalle_comprobante(Integer id_detalle, Integer cantidad) {
 		super();
-		this.detalle_com = detalle_com;
+		this.id_detalle = id_detalle;
 		this.cantidad = cantidad;
 	}
 
-	public Integer getDetalle_com() {
-		return detalle_com;
+	public Integer getId_detalle() {
+		return id_detalle;
 	}
 
-	public void setDetalle_com(Integer detalle_com) {
-		this.detalle_com = detalle_com;
+	public void setId_detalle(Integer id_detalle) {
+		this.id_detalle = id_detalle;
 	}
 
 	public Integer getCantidad() {
@@ -51,9 +56,9 @@ public class Detalle_comprobante implements Serializable {
 		this.cantidad = cantidad;
 	}
 
-	//este metodo nos calculara el importe en cada linea de producto
+	// este metodo nos calculara el importe en cada linea de producto
 	public double calcularImporte() {
-		return  cantidad.doubleValue()*producto.getPrecio();
+		return cantidad.doubleValue() * producto.getPrecio();
 	}
 
 	private static final long serialVersionUID = 1L;
